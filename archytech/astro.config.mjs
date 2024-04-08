1c33ae3bd146e8078e027b078fbed705157f04e9
wgriffey <wgriffey15@gmail.com>
2024-04-08 10:47:31 -0600

    Added netlify form

---
+++ archytech/astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+import robotsTxt from 'astro-robots-txt';
+import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "hybrid",
+  adapter: netlify(),
+  integrations: [react(), tailwind(), partytown(), robotsTxt(), sitemap()],
+});
