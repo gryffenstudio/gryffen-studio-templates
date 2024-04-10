6ac757331a7e89926fdad098ab0b2be97bb9f0ca
wgriffey <wgriffey15@gmail.com>
2024-04-10 12:28:03 -0600

    Added starting files in archytech

---
+++ archytech/sanity.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './schema';
+export default defineConfig({
+    name: 'default',
+    title: 'nurturelume',
+
+    projectId: 'zgzkbg6y',
+    dataset: 'production',
+
+    plugins: [structureTool(), visionTool()],
+
+    schema: {
+        types: schemaTypes,
+    },
+});
---
+++ archytech/src/utils/image.ts
@@ -0,0 +1,16 @@
+import imageUrlBuilder from '@sanity/image-url';
+import type { Image } from '@sanity/types';
+import { createClient } from '@sanity/client';
+
+export const client = createClient({
+    projectId: 'zgzkbg6y',
+    dataset: 'production',
+    useCdn: false,
+    apiVersion: '2024-03-29',
+});
+
+export const imageBuilder = imageUrlBuilder(client);
+
+export function urlFor(source: Image) {
+    return imageBuilder.image(source);
+}
