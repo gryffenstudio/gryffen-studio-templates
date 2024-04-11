f51f01458e3349990032c6b883d113587bafc394
wgriffey <wgriffey15@gmail.com>
2024-04-11 14:24:40 -0600

    Updated the nav bar to change colors based on scroll and if the menu is open. Added background image to hero section

---
+++ archytech/src/scripts/navbar.ts
@@ -0,0 +1,92 @@
+let menu = document.getElementById('menu');
+let menuToggle = document.getElementById('menu-toggle');
+let menuToggleElement = document.getElementById('menu-toggle-element');
+let logo = document.getElementById('logo');
+let isMenuOpen = false;
+let storedLogoColor = logo?.classList.contains('fill-brand-black') ? 'fill-brand-black' : 'fill-white';
+
+
+
+// Function to toggle menu visibility and color
+function toggleMenu() {
+    // Get the current scroll position
+    let scrollPosition = window.scrollY;
+    // Toggle menu visibility
+    menuToggleElement?.classList.toggle('duration-0', true);
+    menu?.classList.toggle('translate-x-full');
+    menu?.classList.toggle('invisible');
+    menuToggle?.classList.toggle('hamburger-toggle');
+
+    // Toggle menu state
+    isMenuOpen = !isMenuOpen;
+
+     // Toggle logo color based on menu state
+    if (isMenuOpen) {
+        logo?.classList.remove(storedLogoColor);
+        logo?.classList.add('fill-white');
+        menuToggleElement?.classList.remove(storedLogoColor === 'fill-brand-black' ? 'bg-brand-black' : 'bg-white');
+        menuToggleElement?.classList.add('bg-white');
+        menuToggleElement?.classList.remove(storedLogoColor === 'fill-brand-black' ? 'before:bg-brand-black' : 'before:bg-white');
+        menuToggleElement?.classList.add('before:bg-white');
+        menuToggleElement?.classList.remove(storedLogoColor === 'fill-brand-black' ? 'after:bg-brand-black' : 'after:bg-white');
+        menuToggleElement?.classList.add('after:bg-white');
+    }
+    else {
+        logo?.classList.remove('fill-white');
+        logo?.classList.add(storedLogoColor);
+        menuToggleElement?.classList.remove('bg-white');
+        menuToggleElement?.classList.add(storedLogoColor === 'fill-brand-black' ? 'bg-brand-black' : 'bg-white');
+        menuToggleElement?.classList.remove('before:bg-white');
+        menuToggleElement?.classList.add(storedLogoColor === 'fill-brand-black' ? 'before:bg-brand-black' : 'before:bg-white');
+        menuToggleElement?.classList.remove('after:bg-white');
+        menuToggleElement?.classList.add(storedLogoColor === 'fill-brand-black' ? 'after:bg-brand-black' : 'after:bg-white');
+    }
+}
+
+function updateLogoAndMenuToggleColors(){
+    // Get the current scroll position
+    let scrollPosition = window.scrollY;
+    // Check if the scroll position is greater than or equal to 850px
+    if (scrollPosition >= 850 && !isMenuOpen) {
+        // Toggle logo color to fill-brand-black
+        logo?.classList.remove('fill-white');
+        logo?.classList.add('fill-brand-black');
+        // Toggle menu toggle color to bg-brand-black
+        menuToggleElement?.classList.remove('bg-white');
+        menuToggleElement?.classList.add('bg-brand-black');
+        menuToggleElement?.classList.remove('before:bg-white');
+        menuToggleElement?.classList.add('before:bg-brand-black');
+        menuToggleElement?.classList.remove('after:bg-white');
+        menuToggleElement?.classList.add('after:bg-brand-black');
+    } else {
+        // Toggle logo color to fill-brand-black
+        logo?.classList.remove('fill-brand-black');
+        logo?.classList.add('fill-white');
+        // Toggle menu toggle color to bg-white
+        menuToggleElement?.classList.remove('bg-brand-black');
+        menuToggleElement?.classList.add('bg-white');
+        menuToggleElement?.classList.remove('before:bg-brand-black');
+        menuToggleElement?.classList.add('before:bg-white');
+        menuToggleElement?.classList.remove('after:bg-brand-black');
+        menuToggleElement?.classList.add('after:bg-white');
+    }
+}
+
+// Event listener for menu toggle button
+document.getElementById('menu-toggle')?.addEventListener('click', function () {
+    let scrollPosition = window.scrollY;
+    // Update storedLogoColor when menu is first opened
+    if (isMenuOpen && scrollPosition >= 850) {
+        storedLogoColor = 'fill-brand-black';
+    }
+    else{
+        storedLogoColor = 'fill-white';
+    }
+    toggleMenu();
+});
+
+
+// Event listener for scroll and load events to update logo and menu toggle colors
+['load', 'scroll'].forEach(event =>
+    window.addEventListener(event, updateLogoAndMenuToggleColors)
+);
