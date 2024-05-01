a37ed44d3f567336c8a19a36019d41b6b3c4e8da
wgriffey <wgriffey15@gmail.com>
2024-05-01 14:15:19 -0600

    Added content collection for projects and loading the data for each project dyanmically

---
+++ archytech/src/content/config.ts
@@ -0,0 +1,31 @@
+import { z, defineCollection } from "astro:content";
+
+const projectCollection = defineCollection({
+    type:'data',
+    schema: ({ image }) => z.object({
+        projectName: z.string().max(100, {
+            message: 'Project name is too long!'
+        }),
+        category: z.enum(['residential', 'commercial', 'in_progress', 'all']),
+        location: z.string(),
+        buildingSize: z.string(),
+        completionYear: z.string(),
+        designBrief: z.string(),
+        heroImageMobile: image(),
+        heroImageDesktop: image(),
+        heroImageAlt: z.string(),
+        designBriefImageMobile: image(),
+        designBriefImageDesktop: image(),
+        designBriefImageAlt: z.string(),
+        interiorImagesMobile: z.array(image()),
+        interiorImagesDesktop: z.array(image()),
+        interiorImageAlt: z.array(z.string()),
+        exteriorImagesMobile: z.array(image()),
+        exteriorImagesDesktop: z.array(image()),
+        exteriorImageAlt: z.array(z.string()),
+    })
+});
+
+export const collections = {
+    'projects': projectCollection
+}
