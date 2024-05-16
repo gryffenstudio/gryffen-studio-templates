485110ccab967bcc4b7940f902ae3bd8cc4624ca
wgriffey <wgriffey15@gmail.com>
2024-05-16 11:48:37 -0600

    made useCDN false again for clients

--- archytech/src/utils/image.ts
+++ archytech/src/utils/image.ts
@@ -5,7 +5,7 @@ import { createClient } from '@sanity/client';
 export const client = createClient({
     projectId: 'bpernsxq',
     dataset: 'production',
-    useCdn: true,
+    useCdn: false,
     apiVersion: '2024-05-14',
 });
 
