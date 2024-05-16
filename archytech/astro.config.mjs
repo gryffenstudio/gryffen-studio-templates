064d3b5c68149bf39bd92f55a36f65efa448722c
wgriffey <wgriffey15@gmail.com>
2024-05-16 12:08:23 -0600

    Allow image optimization from sanity

--- archytech/astro.config.mjs
+++ archytech/astro.config.mjs
@@ -50,6 +50,9 @@ export default defineConfig({
             studioBasePath: '/sanity-studio-admin'
         })
     ],
+    image: {
+        domains: ["cdn.sanity.io"],
+    },
     prefetch: {
         prefetchAll: true,
     },
