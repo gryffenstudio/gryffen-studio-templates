bcc58dcb7c47f9cfa4843160b654b94470526abb
wgriffey <wgriffey15@gmail.com>
2024-07-09 13:48:20 -0600

    removed nurturelume

--- nurturelume/src/scripts/navbar.ts
+++
@@ -1,9 +0,0 @@
-let menu = document.getElementById('menu');
-let menuToggle = document.getElementById('menu-toggle');
-// Event listener for menu toggle button
-document.getElementById('menu-toggle')?.addEventListener('click', function () {
-    // Toggle menu visibility
-    menu?.classList.toggle('translate-x-full');
-    menu?.classList.toggle('invisible');
-    menuToggle?.classList.toggle('hamburger-toggle');
-});
