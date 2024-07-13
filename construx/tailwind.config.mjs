9c97514a8bad44c19336b17b05f552898b36598c
wgriffey <wgriffey15@gmail.com>
2024-07-13 11:53:45 -0600

    Began work on the navbar and set the layout styling

--- construx/tailwind.config.mjs
+++ construx/tailwind.config.mjs
@@ -3,19 +3,19 @@ export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {},
-    },
-    container: {
-        center: true,
-        padding: {
-            DEFAULT: '24px',
-            md: '80px',
-        },
-        screens: {
-            sm: '640px',
-            md: '768px',
-            lg: '1024px',
-            xl: '1280px',
-            '2xl': '1440px',
+        container: {
+            center: true,
+            // padding: {
+            //     DEFAULT: '24px',
+            //     md: '80px',
+            // },
+            screens: {
+                sm: '640px',
+                md: '768px',
+                lg: '1024px',
+                xl: '1280px',
+                '2xl': '1440px',
+            },
         },
     },
     plugins: [],
